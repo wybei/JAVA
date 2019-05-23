28e5aa226c4fdc9563ac67ceaecdfbfca4918d5a
wybei <734831833@qq.com>
2019-05-23 19:12:16 +0800

    希尔排序

---
+++ sort/src/com/wyb/sort/ShellSort.java
@@ -0,0 +1,29 @@
+package com.wyb.sort;
+
+import java.util.Arrays;
+
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] arr = new int[] {9,1,2,5,7,4,8,6,3,5};
+        System.out.println(Arrays.toString(arr));
+        shellSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    private static void shellSort(int[] arr){
+        // 遍历所有的步长
+        for (int d = arr.length / 2; d > 0; d /= 2) {
+            // 遍历所有有元素
+            for (int i = d; i < arr.length; i++) {
+                // 遍历本组中所有的元素
+                for (int j = i-d; j >= 0; j -= d) {
+                    // 如果当前元素大于加上步长后的那个元素
+                    if (arr[j] > arr[j + d]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j + d];
+                        arr[j + d] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
